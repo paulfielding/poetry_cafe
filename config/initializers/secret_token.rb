be24f5d6f98ac37a87d6473ebc14811e8076bb6d
paulfielding <paulfielding@aciddevelopments.co.uk>
2012-05-18 23:20:24 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Poetrycafe::Application.config.secret_token = 'fe340648b822966a9e307acc0cc56e2ed4e1e8535d8e35211f2f3b2392347e870b23f7b5afa58a20cf22a93398eed444ea918729702a35e663c0a81e6edcf837'
